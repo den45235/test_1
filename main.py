b61b236979a4028b9c3a15fbba51a7fdd284128e
den45235 <zed8iz2x@gmail.com>
2022-01-11 16:20:55 +0300

    2

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-def plus(a, b)
+def plus(a, b):
     return a + b
 
-if __name__ == '__main'
+if __name__ == '__main__':
     print(plus(2, 2))
