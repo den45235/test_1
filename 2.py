16cb3d2a54c77e039ed1922d6543afe7e920e10e
den45235 <zed8iz2x@gmail.com>
2022-01-11 16:36:51 +0300

    add 2

---
+++ 2.py
@@ -0,0 +1,5 @@
+def frgb(a, b):
+    return a * b
+
+if __name__ == '__main__':
+    print(frgb(3, 7))
--- main.py
+++ main.py
@@ -17,5 +17,9 @@ if __name__ == '__main__':
 def plus(a, b):
     return a + b
 
+def minus(a, b):
+    return a - b
+
 if __name__ == '__main__':
     print(plus(2, 2))
+    print(minus(2, 2))
